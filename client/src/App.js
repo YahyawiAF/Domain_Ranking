615e634f5a4bd4ed564ffefd2a946d2dad15b331
YahyawiAF <33427715+YahyawiAF@users.noreply.github.com>
2021-06-01 13:13:25 +0200

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+import RouterConfig from "./navigation/RouterConfig";
+
+import "./index.css";
+
+const App = () => {
+  return <RouterConfig />;
+};
+
+export default App;
